04d4e1ee0ac1e3759594a8a4426eae65d1e2affc
entropydec <2711364531@qq.com>
2022-11-18 14:22:50 +0800

    add a file

---
+++ src/MakeScripts.py
@@ -0,0 +1,11 @@
+class MakeScripts:
+
+    def __init__(self,transition):
+        self.transition=transition
+        self.var='d'
+
+    def get_scripts(self):
+        scripts=[]
+        location=self.var+'.'
+
+    
